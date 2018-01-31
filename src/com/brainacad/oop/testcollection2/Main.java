ae28e785661ab34a053549e5a2a80940b4ed934e
romcha68 <romcha68@ukr.net>
2018-01-31 12:39:24 +0200

    add randomaizer

--- src/com/brainacad/oop/testcollection2/Main.java
+++ src/com/brainacad/oop/testcollection2/Main.java
@@ -9,7 +9,14 @@ public class Main {
         Random random = new Random();
 
         for(int i = 0; i < 10; i++){
-            list.add(random.nextInt(i+1), "num_" + i);
+            int index = random.nextInt(i+1);
+            while (true){
+                int element = random.nextInt(11);
+                if(!list.contains(Integer.toString(element))){
+                    list.add(index,Integer.toString(element));
+                    break;
+                }
+            }
         }
         Iterator iterator = list.iterator();
         while (iterator.hasNext()) {
